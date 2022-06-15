07c564005290a215084f8c5751edbeeab8eddbe1
binohub <yeebino001@gmail.com>
2022-06-15 17:04:45 +0200

    ms1 - finish

--- main.js
+++ main.js
@@ -1,23 +1,10 @@
 
-//! variabili globali
-const ticketPrice = 0.26;
-console.log(`prezzo giornaliero: ${ticketPrice} al km`);
-
-
-
-
-
-
-
-// if (isNaN(dudeAge)){
-//     alert("digita un numero valido!");
-// }
-
-
-
 const dudeGenerate = document.getElementById("dude-generate");
 dudeGenerate.addEventListener("click", function () {
 
+    //! variabili standard
+    const ticketPrice = 0.26;
+    console.log(`prezzo giornaliero: ${ticketPrice} al km`);
     //! input km
     const dudeKm = parseInt(document.getElementById("dude-km").value);
     if (isNaN(dudeKm)) {
@@ -30,25 +17,24 @@ dudeGenerate.addEventListener("click", function () {
         alert("digita un età valida!");
     }
     console.log(`età: ${dudeAge}`);
-
     //! output ticketFullPrice
     const ticketFullPrice = ticketPrice * dudeKm;
     console.log(`prezzo totale: ${ticketFullPrice.toFixed(2)}€`);
-
     //! output sale
     if (dudeAge < 18) {
-        const dudeSale = (ticketFullPrice * 20) / 100;
-        console.log(`minorenne: sconto del 20% (${dudeSale}€)`);
+        const dudeSale = (ticketFullPrice * 15) / 100;
+        console.log(`minorenne: sconto del 15% (${dudeSale.toFixed(2)}€)`);
         const ticketFinalPrice = ticketFullPrice - dudeSale;
         console.log(`prezzo scontato: ${ticketFinalPrice.toFixed(2)}€`);
     } else if (dudeAge > 65) {
-        const dudeSale = parseInt((ticketFullPrice * 40) / 100);
-        console.log(`over 65: sconto del 40% (${dudeSale}€)`);
+        const dudeSale = parseInt((ticketFullPrice * 35) / 100);
+        console.log(`over 65: sconto del 35% (${dudeSale.toFixed(2)}€)`);
         const ticketFinalPrice = ticketFullPrice - dudeSale;
         console.log(`prezzo scotato: ${ticketFinalPrice.toFixed(2)}€! 🤯`);
     } else {
         console.log(`prezzo NON scontabile: ${ticketFullPrice.toFixed(2)}€`);
     }
+    
 });
 
 // document.getElementById("ticket-full-price").innerHTML = (`il prezzo totale è di ${ticketFullPrice.toFixed(2)}€ 💸`);
