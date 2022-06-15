abbf3004f137927734841a368ba66c053c61045a
binohub <yeebino001@gmail.com>
2022-06-15 16:38:32 +0200

    ms1 - update

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 
 //! variabili globali
-const ticketPrice = 0.21;
+const ticketPrice = 0.26;
 // document.getElementById("ticket-price").innerHTML = (`oggi il prezzo è di soli ${ticketPrice} a km! 😃 `);
 
 //! numero di km che vuole percorrere
 const dudeKm = document.getElementById("dude-km").value;
+//! ticketFullPrice
+const ticketFullPrice = ticketPrice * dudeKm;
+
 
 // if (isNaN(dudeKm)){
 //     alert("digita un numero valido!");
@@ -16,30 +19,32 @@ const dudeAge = document.getElementById("dude-age").value;
 //     alert("digita un numero valido!");
 // }
 
-//! ticketFullPrice
+
 
 const dudeGenerate = document.getElementById("dude-generate");
 dudeGenerate.addEventListener("click", function() {
-    const ticketFullPrice = ticketPrice * dudeKm;
+
     console.log(`il prezzo totale è di ${ticketFullPrice.toFixed(2)}€`);
+
+    //! se età minore di 18
+    if (dudeAge < 18){
+        const dudeSale = (ticketFullPrice * 20) / 100;
+        console.log(`per i minorenni abbiamo uno sconto del 20% nel suo caso di ${dudeSale}€`);
+        const ticketFinalPrice = ticketFullPrice - dudeSale;
+        console.log(`il suo prezzo scende a ${ticketFinalPrice.toFixed(2)}€`);
+    }else if (dudeAge > 65){
+        const dudeSale = parseInt((ticketFullPrice * 40) / 100);
+        console.log(`per gli over 65 abbiamo uno sconto del 40% nel suo caso di ${dudeSale}€`);
+        const ticketFinalPrice = ticketFullPrice - dudeSale;
+        console.log(`il suo prezzo scende a ${ticketFinalPrice.toFixed(2)}€! 🤯`);
+    }else{
+        console.log(`non abbiamo sconti per lei 😢`);
+        console.log(`come detto in precedenza il prezzo è di ${ticketFullPrice.toFixed(2)}€`);}
 });
 
 // document.getElementById("ticket-full-price").innerHTML = (`il prezzo totale è di ${ticketFullPrice.toFixed(2)}€ 💸`);
 
 
-//se età minore di 18
-// if (dudeAge < 18){
-//     const dudeSale = (ticketFullPrice * 20) / 100;
-//     document.getElementById("dude-sale").innerHTML = (`per i minorenni abbiamo uno sconto del 20% nel suo caso di ${dudeSale}€ `);
-//     const ticketFinalPrice = ticketFullPrice - dudeSale;
-//     document.getElementById("ticket-final-price").innerHTML = (`il suo prezzo scende a ${ticketFinalPrice.toFixed(2)}€!🤯`);
-// }else if (dudeAge > 65){
-//     const dudeSale = parseInt((ticketFullPrice * 40) / 100);
-//     document.getElementById("dude-sale").innerHTML = (`per gli over 65 abbiamo uno sconto del 40% nel suo caso di ${dudeSale}€`);
-//     const ticketFinalPrice = ticketFullPrice - dudeSale;
-//     document.getElementById("ticket-final-price").innerHTML = (`il suo prezzo scende a ${ticketFinalPrice.toFixed(2)}€! 🤯`);
-// }else{
-//     document.getElementById("dude-sale").innerHTML = (`non abbiamo sconti per lei 😢`);
-//     document.getElementById("ticket-final-price").innerHTML = (`come detto in precedenza il prezzo è di ${ticketFullPrice.toFixed(2)}€`);
+
 // }
 
